f3c39b081b5b3c5b46aaf78b467cbc55be7b485c
poluianov24 <poluianov.vlad.24@gmail.com>
2024-01-19 23:57:27 +0200

    ex-1 done

---
+++ main.py
@@ -0,0 +1,9 @@
+'''Напишіть функцію, яка відобразить на екрані такий відформатований текст:
+    "Don't let the noise of others' opinions
+    drown out your own inner voice."
+    Steve Jobs'''
+def Steve_Jobs():
+    print('"Don\'t let the noise of others\' opinions')
+    print('drown out your own inner voice."')
+    print('\033[33mSteve Jobs')
+Steve_Jobs()
